a01696d66dd5aa07dcab3cabaa551788463d61b1
Balaaditya04 <aditya.taninki@gmail.com>
2025-06-20 09:58:50 +0530

    commit changes

---
+++ JavaScript/file.js
@@ -0,0 +1,28 @@
+// console.log("Hello World");
+let name = "Aditya";
+let age=21
+let promoted=true
+let score
+let city=null
+// console.log(name);
+// console.log(typeof name);
+// console.log(score);
+// console.log(typeof score);
+
+// type casting
+
+// console.log(typeof age)
+// let strAge= String(age)
+// console.log(typeof strAge)
+
+// let rating="5";
+// console.log(typeof rating);
+// let strRating = Number(rating);
+// console.log(typeof strRating)
+
+/*
+let flag="true";
+let boolFlag= Boolean(flag);
+console.log(typeof boolFlag);
+*/
+
