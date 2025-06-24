ba43a6d251ead956fb335a32c1f25d1927d48d27
Balaaditya04 <aditya.taninki@gmail.com>
2025-06-24 22:35:46 +0530

    commit changes

--- JavaScript/Shopping Cart/app.js
+++ JavaScript/Shopping Cart/app.js
@@ -6,6 +6,7 @@ const products = [
     { id: 3, name: "Product3", price: 40 },
 ];
 
+// Show all products
 const showProducts = () => {
     let str = "";
     products.map((value) => {
@@ -15,36 +16,59 @@ const showProducts = () => {
     console.log(str);
 };
 
-
+// Add item to cart (increases quantity if already present)
 const addToCart = (id) => {
-    cart = { ...cart, [id]: 1 };
+    cart = { ...cart, [id]: (cart[id] || 0) + 1 };
+    console.log(`${products.find(p => p.id === id).name} added to cart.`);
 };
 
-
+// Show cart contents
 const showCart = () => {
     console.log("*** My Cart ***");
     let str = "";
     products.map((value) => {
-        cart[value.id] &&
-        (str += `${value.name} - ${value.price} - Quantity: ${cart[value.id]}\n`);
+        if (cart[value.id]) {
+            str += `${value.name} - ₹${value.price} - Quantity: ${cart[value.id]}\n`;
+        }
     });
     console.log(str || "Cart is Empty");
 };
 
-
+// Increase item quantity
 const increment = (id) => {
     cart = { ...cart, [id]: (cart[id] || 0) + 1 };
+    console.log(`${products.find(p => p.id === id).name} quantity increased.`);
 };
+
+// Decrease item quantity, remove if zero
 const decrement = (id) => {
-    cart = { ...cart, [id]: (cart[id] || 0) - 1 };
+    if (cart[id]) {
+        const newQty = cart[id] - 1;
+        if (newQty <= 0) {
+            const { [id]: _, ...rest } = cart;
+            cart = rest;
+            console.log(`${products.find(p => p.id === id).name} removed from cart.`);
+        } else {
+            cart = { ...cart, [id]: newQty };
+            console.log(`${products.find(p => p.id === id).name} quantity decreased.`);
+        }
+    }
 };
 
-
+// Calculate total order value
 const orderValue = () => {
     return products.reduce((sum, value) => {
-        return sum + (value.price * cart[value.id] ?? 0);
+        return sum + (value.price * (cart[value.id] || 0));
     }, 0);
 };
 
-console.log(`Order Value: ${orderValue()}`);
-
+// Example Usage:
+showProducts();
+addToCart(1);
+addToCart(2);
+increment(1);
+showCart();
+console.log(`Order Value: ₹${orderValue()}`);
+decrement(1);
+showCart();
+console.log(`Order Value: ₹${orderValue()}`);
--- JavaScript/file16.js
+++ JavaScript/file16.js
@@ -1,11 +1,79 @@
 //promise 
-const f1=() => {
-    setTimeout(() => {
-    return 5;
-    },1000);
+// const f1=() => {
+//     setTimeout(() => {
+//     return 5;
+//     },1000);
+// };
+// const f2=(x)=>{
+//     console.log(x+6);
+// };
+// const n=f1()
+// f2(n);
+
+
+
+// const f1=() => {
+//     return new Promise((resolve, reject) => {
+//             resolve(5);
+//     });
+// };
+// const f2=(x)=>{
+//     console.log(x+6);
+// };
+// f1().then((n) => f2(n));
+
+
+// const f1=(a) => {
+//     return new Promise((resolve, reject) => {
+//             if(a < 0) reject("Negative number not allowed");{
+//                 else resolve(5);
+//                 reject("Error occurred");
+//             }
+//     });
+// };
+// const f2=(x)=>{
+//     console.log(x+6);
+// };
+// // f1().then((n) => f2(n));
+// f1()
+//     .then((n) => f2(n))
+//     .catch((error) => console.error(error));
+
+// const f1 = (a) => {
+//     return new Promise((resolve, reject) => {
+//         if (a < 0) {
+//             reject("Negative number not allowed");
+//         } else {
+//             resolve(5);
+//         }
+//     });
+// };
+
+// const f2 = (x) => {
+//     console.log(x + 6);
+// };
+
+// f1(3)   
+//     .then((n) => f2(n))
+//     .catch((error) => console.error(error));
+
+
+// fetch("https://jsonplaceholder.typicode.com/users1")
+//     .then((res) => res.json())
+//     .then((data) => {
+//         data.forEach((value) =>{
+//             console.log(value.name);
+//             console.log(value.email);
+//         });
+//     })
+//     .catch((err) => console.log("something went wrong"));
+
+const fetchData = async () => {
+        const res = await fetch("https://jsonplaceholder.typicode.com/users")
+        const data = await res.json();
+        data.forEach((value) => {
+            console.log(value.name);
+        });
 };
-const f2=(x)=>{
-    console.log(x+4);
-};
-const n=f1()
-f2(n);
+fetchData();
+
