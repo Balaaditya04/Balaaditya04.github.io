78843ce8b72cc5909ca65991a9bcc511cc10c96e
Balaaditya04 <aditya.taninki@gmail.com>
2025-06-25 16:46:34 +0530

    commit changes

---
+++ JavaScript/mongoDB/qr3.js
@@ -0,0 +1,12 @@
+//db.employees.insertOne({
+//     name:"John Smith".
+//     email:"john@gmail.com",
+//     department:"IT".
+//     salary: 1456,
+//     location: ["FL", "OH"],
+//     date: Date()
+// })
+//db.employees
+//db.employee.find().sort({name:1})
+// db.employee.find()
+//
